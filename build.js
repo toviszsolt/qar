ad5cd29d65a21dbf9e01721af12612a30b7ac047
Zsolt Tovis <tovis.zsolt@gmail.com>
2026-02-16 22:33:55 +0100

    refactor: improve build script structure and enhance file handling

--- build.js
+++ build.js
@@ -1,51 +1,76 @@
 import esbuild from 'esbuild';
-import fs from 'fs';
-import path from 'path';
+import fs, { readdirSync, readFileSync, statSync, writeFileSync } from 'fs';
+import path from 'path/posix';
 
-const isWatch = process.argv.includes('--watch');
-const outDir = 'dist';
-const common = {
-  bundle: true,
-  minify: true,
-  sourcemap: false,
+const pathSrc = 'src';
+const pathOutput = 'dist';
+const withWatch = process.argv.includes('--watch');
+const commonConfig = { bundle: true, minify: true, sourcemap: false };
+
+const cleanBuild = () => {
+  if (fs.existsSync(pathOutput)) {
+    fs.rmSync(pathOutput, { recursive: true, force: true });
+  }
+
+  fs.mkdirSync(pathOutput);
+  console.log(`[Build] Clean directory: ${path.sep}${pathOutput}`);
 };
 
 const runBuild = async (config) => {
-  if (isWatch) {
+  if (withWatch) {
     const ctx = await esbuild.context(config);
     await ctx.watch();
-    console.log(`Watching: ${config.outfile || config.entryPoints}`);
+    console.log(`[Build] Watching: ${path.sep}${config.outfile || config.entryPoints}`);
   } else {
-    console.log(`Building: ${config.outfile || config.entryPoints}`);
+    console.log(`[Build] Building: ${path.sep}${config.outfile || config.entryPoints}`);
     await esbuild.build(config);
   }
-
-  fs.copyFileSync('src/qar.d.ts', path.join(outDir, 'qar.d.ts'));
 };
 
-if (!isWatch) {
-  fs.rmSync(outDir, { recursive: true, force: true });
-}
+const createLLMBundle = () => {
+  const getFiles = (dir, files = []) => {
+    readdirSync(dir).forEach((f) => {
+      const filePath = path.join(dir, f);
+      statSync(filePath).isDirectory() ? getFiles(filePath, files) : f.endsWith('.js') && files.push(filePath);
+    });
+    return files;
+  };
 
-(async () => {
-  ['esm', 'cjs'].forEach(async (format) => {
+  const bundleFileName = 'bundle-llm.js.txt';
+  const bundle = getFiles(pathSrc)
+    .map((f) => `// ${f}\n${readFileSync(f, 'utf-8')}`)
+    .join('\n\n');
+
+  writeFileSync(bundleFileName, bundle, 'utf-8');
+  console.log(`[Build] Building: ${path.sep}${bundleFileName}`);
+};
+
+const copyTypeDefinitions = () => {
+  const src = path.join(pathSrc, 'qar.d.ts');
+  const dest = path.join(pathOutput, 'qar.d.ts');
+  fs.copyFileSync(src, dest);
+  console.log(`[Build] Copy definitions: ${path.sep}${dest}`);
+};
+
+try {
+  if (!withWatch) {
+    cleanBuild();
+    copyTypeDefinitions();
+    createLLMBundle();
+  }
+
+  for (const format of ['esm', 'cjs']) {
+    const basename = format === 'esm' ? 'qar.js' : 'qar.cjs';
     await runBuild({
-      ...common,
-      entryPoints: ['src/qar.js'],
-      outfile: path.join(outDir, `qar.${format === 'esm' ? 'js' : 'cjs'}`),
+      ...commonConfig,
+      entryPoints: [path.join(pathSrc, 'qar.js')],
+      outfile: path.join(pathOutput, basename),
       format: format,
       platform: 'node',
-      target: ['node18'],
       legalComments: 'none',
     });
-  });
-
-  await runBuild({
-    ...common,
-    entryPoints: ['src/qar.js'],
-    outfile: 'bundle-llm.js.txt',
-    format: 'esm',
-    minify: false,
-    platform: 'node',
-  });
-})();
+  }
+} catch (err) {
+  console.error('[Build] Failed:', err);
+  process.exit(1);
+}
